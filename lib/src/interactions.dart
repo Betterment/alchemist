f4c780ff52869d9548019d95fe660e5b4bb65ac3
Kirpal Demian <kirpal@verygood.ventures>
2022-02-18 17:39:18 -0500

    fix: tap gesture not showing correctly in golden file

--- lib/src/interactions.dart
+++ lib/src/interactions.dart
@@ -6,18 +6,22 @@ import 'package:flutter_test/flutter_test.dart';
 /// An interaction to perform while rendering a golden test. Returns
 /// an asynchronous callback that should be called to cleanup when
 /// the golden test completes.
-typedef Interaction = Future<AsyncCallback> Function(WidgetTester);
+typedef Interaction = Future<AsyncCallback?> Function(WidgetTester);
 
 /// Presses all widgets matching [finder].
-Interaction press(Finder finder) => (WidgetTester tester) async {
+Interaction press(Finder finder) => (
+      WidgetTester tester, {
+      Duration? holdFor = const Duration(milliseconds: 300),
+    }) async {
       final gestures = await tester.pressAll(finder);
-      await tester.pump();
+      await tester.pump(kPressTimeout);
+      await tester.pump(holdFor);
       return gestures.releaseAll;
     };
 
 /// Long-presses all widgets matching [finder].
 Interaction longPress(Finder finder) => (WidgetTester tester) async {
       final gestures = await tester.pressAll(finder);
-      await tester.pump(kLongPressTimeout + kPressTimeout);
+      await tester.pump(kLongPressTimeout);
       return gestures.releaseAll;
     };
--- test/interactions_test.dart
+++ test/interactions_test.dart
@@ -3,48 +3,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('press', () {
-    Widget buildWrapper(Widget child) {
-      return MaterialApp(
-        home: Scaffold(
-          body: child,
-        ),
-      );
-    }
+  Widget buildWrapper(Widget child) {
+    return MaterialApp(
+      home: Scaffold(
+        body: child,
+      ),
+    );
+  }
 
-    testWidgets('presses', (tester) async {
-      var onPressedCalled = false;
-      await tester.pumpWidget(
-        buildWrapper(
-          ElevatedButton(
-            onPressed: () {
-              onPressedCalled = true;
-            },
-            child: const Text('button'),
-          ),
+  testWidgets('press', (tester) async {
+    var onPressedCalled = false;
+    await tester.pumpWidget(
+      buildWrapper(
+        ElevatedButton(
+          onPressed: () {
+            onPressedCalled = true;
+          },
+          child: const Text('button'),
         ),
-      );
-      final cleanup = await press(find.byType(ElevatedButton))(tester);
-      await cleanup();
-      expect(onPressedCalled, isTrue);
-    });
+      ),
+    );
+    final cleanup = await press(find.byType(ElevatedButton))(tester);
+    await cleanup?.call();
+    expect(onPressedCalled, isTrue);
+  });
 
-    testWidgets('longPresses', (tester) async {
-      var onLongPressedCalled = false;
-      await tester.pumpWidget(
-        buildWrapper(
-          ElevatedButton(
-            onPressed: null,
-            onLongPress: () {
-              onLongPressedCalled = true;
-            },
-            child: const Text('button'),
-          ),
+  testWidgets('longPress', (tester) async {
+    var onLongPressedCalled = false;
+    await tester.pumpWidget(
+      buildWrapper(
+        ElevatedButton(
+          onPressed: null,
+          onLongPress: () {
+            onLongPressedCalled = true;
+          },
+          child: const Text('button'),
         ),
-      );
-      final cleanup = await longPress(find.byType(ElevatedButton))(tester);
-      await cleanup();
-      expect(onLongPressedCalled, isTrue);
-    });
+      ),
+    );
+    final cleanup = await longPress(find.byType(ElevatedButton))(tester);
+    await cleanup?.call();
+    expect(onLongPressedCalled, isTrue);
   });
 }
