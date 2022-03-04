b4f6c55e89ef84edb31523ad3742c68f0373a96d
Joanna May <joannamadisonmay@gmail.com>
2022-03-04 14:07:34 -0600

    Don't run platform tests if run by GH actions

--- test/src/flutter_test_config.dart
+++ test/src/flutter_test_config.dart
@@ -1,14 +1,22 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  final enablePlatformTests =
+      !Platform.environment.containsKey('GITHUB_ACTIONS');
+
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig(
       theme: ThemeData(
         textTheme: const TextTheme().apply(fontFamily: 'Roboto'),
       ),
+      platformGoldensConfig:
+          AlchemistConfig.current().platformGoldensConfig.copyWith(
+                enabled: enablePlatformTests,
+              ),
     ),
     run: testMain,
   );
