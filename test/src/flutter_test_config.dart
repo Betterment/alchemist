9a63d42a0ec4f79f832498a44a050b325aae1b4e
Joanna May <joannamadisonmay@gmail.com>
2022-03-04 14:17:28 -0600

    Debug ci some more

--- test/src/flutter_test_config.dart
+++ test/src/flutter_test_config.dart
@@ -5,10 +5,18 @@ import 'package:alchemist/alchemist.dart';
 import 'package:flutter/material.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
+  // ignore: avoid_print
   print('My environment from CI');
+  // ignore: avoid_print
   print(Platform.environment);
+  for (final key in Platform.environment.keys) {
+    // ignore: avoid_print
+    print('$key: ${Platform.environment[key]}');
+  }
   final enablePlatformTests =
       !Platform.environment.containsKey('GITHUB_ACTIONS');
+  // ignore: avoid_print
+  print('Enable platform tests: $enablePlatformTests');
 
   return AlchemistConfig.runWithConfig(
     config: AlchemistConfig(
