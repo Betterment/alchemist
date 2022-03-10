816e4662abae5e80cea90ce84fa8eefca26df3bd
Giuseppe Cianci <giuseppe.cianci97@gmail.com>
2022-03-10 12:07:01 +0100

    add smoke test

--- test/smoke_tests/shadows_smoke_test.dart
+++ test/smoke_tests/shadows_smoke_test.dart
@@ -1,1 +1,50 @@
-// TODO
+import 'dart:async';
+
+import 'package:alchemist/alchemist.dart';
+import 'package:alchemist/src/golden_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class ShadowWidget extends StatelessWidget {
+  const ShadowWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.red,
+            spreadRadius: 10,
+            blurRadius: 10,
+          )
+        ],
+      ),
+      child: const Text('text'),
+    );
+  }
+}
+
+void main() {
+  final ciConfigRenderShadowsEnabled =
+      AlchemistConfig.current().ciGoldensConfig.copyWith(renderShadows: true);
+
+  AlchemistConfig.runWithConfig(
+    config: AlchemistConfig.current()
+        .copyWith(ciGoldensConfig: ciConfigRenderShadowsEnabled),
+    run: () {
+      goldenTest(
+        'renders shadows',
+        fileName: 'shadows_smoke_test_with_shadows_config',
+        widget: const ShadowWidget(),
+      );
+    },
+  );
+  group('smoke test - renderShadows disabled', () {
+    goldenTest(
+      'succeeds after tapping button with timer',
+      fileName: 'shadows_smoke_test_default_ci_config',
+      widget: const ShadowWidget(),
+    );
+  });
+}
