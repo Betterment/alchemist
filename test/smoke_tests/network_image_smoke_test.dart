e68dfd4190e16214733d12a672f942ded507e006
Joanna May <joannamadisonmay@gmail.com>
2022-03-15 12:56:07 -0500

    Clear test image cache before running network_image_smoke_test

--- test/smoke_tests/network_image_smoke_test.dart
+++ test/smoke_tests/network_image_smoke_test.dart
@@ -1,15 +1,47 @@
+import 'dart:typed_data';
+
 import 'package:alchemist/alchemist.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail_image_network/mocktail_image_network.dart';
-import 'package:test/test.dart';
+
+class NetworkImageAssetBundleThatDoesNothing extends TestAssetBundle {
+  @override
+  Future<ByteData> load(String key) async {
+    return ByteData(0);
+  }
+}
+
+final imageCache = ImageCache();
+
+class CustomWidgetsBinding extends AutomatedTestWidgetsFlutterBinding {
+  @override
+  ImageCache createImageCache() {
+    return imageCache;
+  }
+
+  static WidgetsBinding ensureInitialized() => CustomWidgetsBinding();
+}
 
 void main() {
+  // We need to clear out images from the cache, which requires us to
+  // initialize a custom widgets binding.
+  CustomWidgetsBinding.ensureInitialized();
+
   group('smoke test', () {
     goldenTest(
       'succeeds with a network image',
       fileName: 'network_image_smoke_test',
       pumpWidget: (tester, widget) async {
-        await mockNetworkImages(() => tester.pumpWidget(widget));
+        imageCache.clear();
+        await mockNetworkImages(
+          () => tester.pumpWidget(
+            DefaultAssetBundle(
+              bundle: NetworkImageAssetBundleThatDoesNothing(),
+              child: widget,
+            ),
+          ),
+        );
       },
       builder: () => Image.network('https://fakeurl.com/image.png'),
     );
