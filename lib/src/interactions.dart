0d7c1c30cd6c112eb14d3f767609fdc9734e03c2
Marcos Sevilla <marcossevilla14@gmail.com>
2022-03-17 11:20:44 -0600

    feat: add scroll interaction

--- lib/src/interactions.dart
+++ lib/src/interactions.dart
@@ -6,9 +6,9 @@ import 'package:flutter_test/flutter_test.dart';
 /// An interaction to perform while rendering a golden test. Returns
 /// an asynchronous callback that should be called to cleanup when
 /// the golden test completes.
-typedef Interaction = Future<AsyncCallback?> Function(WidgetTester);
+typedef Interaction = Future<AsyncCallback> Function(WidgetTester);
 
-/// Presses all widgets matching [finder].
+/// Presses all widgets matching `finder`.
 Interaction press(
   Finder finder, {
   Duration? holdFor = const Duration(milliseconds: 300),
@@ -25,4 +25,19 @@ Interaction longPress(Finder finder) => (WidgetTester tester) async {
       final gestures = await tester.pressAll(finder);
       await tester.pump(kLongPressTimeout);
       return gestures.releaseAll;
+    };
+
+/// Scrolls all widgets matching `finder`.
+Interaction scroll(
+  Finder finder, {
+  required Offset offset,
+  double speed = kMinFlingVelocity,
+}) =>
+    (WidgetTester tester) async {
+      final elements = finder.evaluate();
+      return () async {
+        for (final element in elements) {
+          await tester.fling(find.byWidget(element.widget), offset, speed);
+        }
+      };
     };
--- test/src/interactions_test.dart
+++ test/src/interactions_test.dart
@@ -1,6 +1,7 @@
 import 'package:alchemist/src/interactions.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -26,7 +27,7 @@ void main() {
         ),
       );
       final cleanup = await press(find.byType(ElevatedButton))(tester);
-      await cleanup?.call();
+      await cleanup.call();
       expect(onPressedCalled, isTrue);
     });
 
@@ -40,16 +41,16 @@ void main() {
         ),
       );
       const holdForDuration = Duration(seconds: 3);
-      // Since testWidgets runs inside a fakeAsync context, we can grab the
-      // fake starting time. Pumping for a certain duration advances the
-      // fake test clock, allowing us to verify we are pumping correctly
+      // Since `testWidgets` runs inside a `fakeAsync` context, we can grab the
+      // fake starting time. Pumping for a certain [Duration] advances the
+      // fake test `clock`, allowing us to verify we are pumping correctly
       // in the interactions code.
       final startTime = tester.binding.clock.now();
       final cleanup = await press(
         find.byType(ElevatedButton),
         holdFor: holdForDuration,
       )(tester);
-      await cleanup?.call();
+      await cleanup.call();
       final elapsed = tester.binding.clock.now().difference(startTime);
       expect(elapsed, greaterThanOrEqualTo(holdForDuration + kPressTimeout));
     });
@@ -69,7 +70,38 @@ void main() {
       ),
     );
     final cleanup = await longPress(find.byType(ElevatedButton))(tester);
-    await cleanup?.call();
+    await cleanup.call();
     expect(onLongPressedCalled, isTrue);
+  });
+
+  testWidgets('scroll', (tester) async {
+    await tester.pumpWidget(
+      buildWrapper(
+        ListView.builder(
+          // this `itemCount` is long enough to reach the `dragOffset`
+          // we're using on our scroll interaction.
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('item $index'),
+            );
+          },
+        ),
+      ),
+    );
+
+    const dragOffset = 100.0;
+
+    final cleanup = await scroll(
+      find.byType(Scrollable),
+      offset: const Offset(0, -dragOffset),
+    )(tester);
+
+    await cleanup.call();
+
+    final viewportFinder = find.byType(Viewport);
+    final viewport = tester.renderObject(viewportFinder) as RenderViewport;
+
+    expect(viewport.offset.pixels, dragOffset);
   });
 }
