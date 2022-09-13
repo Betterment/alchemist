0cc2a3774597af64524fd8b20bc07e209814bfdd
Kirpal Demian <demian@kirp.al>
2022-09-13 11:39:48 -0600

    fix: ensure that error messages are always legible (#78)

--- lib/src/golden_test.dart
+++ lib/src/golden_test.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
+import 'dart:ui' as ui;
 
 import 'package:alchemist/alchemist.dart';
 import 'package:alchemist/src/alchemist_test_variant.dart';
 import 'package:alchemist/src/golden_test_runner.dart';
 import 'package:alchemist/src/utilities.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:meta/meta.dart';
@@ -21,6 +23,10 @@ set goldenTestRunner(GoldenTestRunner value) => _goldenTestRunner = value;
 /// golden tests.
 Future<void> _setUpGoldenTests() async {
   await loadFonts();
+
+  RenderErrorBox.textStyle = ui.TextStyle(
+    fontFamily: 'Roboto',
+  );
 }
 
 /// Loads a font for use in golden tests.
---
+++ test/smoke_tests/error_message_smoke_test.dart
@@ -0,0 +1,19 @@
+import 'package:alchemist/src/golden_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('smoke test', () {
+    goldenTest(
+      'succeeds with an error message',
+      fileName: 'error_message_smoke_test',
+      builder: () => SizedBox(
+        width: 200,
+        height: 200,
+        child: ErrorWidget(
+          FlutterError('This is an error message.'),
+        ),
+      ),
+    );
+  });
+}
